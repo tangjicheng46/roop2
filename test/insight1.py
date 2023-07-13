90ed25dfbdbb6bde07aed351b19540ec85a883c3
tangjicheng <tangjch15@gmail.com>
2023-07-13 13:31:43 +0000

    new file:   test/insight1.py

---
+++ test/insight1.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import insightface
+from insightface.app import FaceAnalysis
+from insightface.data import get_image as ins_get_image
+
+import cv2
+
+app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640, 640))
+img = cv2.imread("./image/song1.jpg")
+faces = app.get(img)
+rimg = app.draw_on(img, faces)
+cv2.imwrite("./t1_output.jpg", rimg)
