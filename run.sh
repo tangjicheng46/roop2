1dadd987068e783c1b3e9ae2022e4ce5c88f130e
tangjicheng <tangjch15@gmail.com>
2023-07-14 18:06:18 +0000

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 python roop2.py --input-image ./image/song1.jpg --input-video ./input/i1.mp4 --output-video ./output/o1.mp4
+
+python roop2.py --input-image ./image/song1.jpg --input-video ./input/i2.mp4 --output-video ./output/o2.mp4
